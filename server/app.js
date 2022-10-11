3566946ea1c259e32227f4208046023c737d8ace
Guillermo Andrada <guillermoandrada@gmail.com>
2022-10-11 12:43:56 -0300

    favicon y ajustes de la app

--- server/app.js
+++ server/app.js
@@ -7,18 +7,21 @@ app.use(fileUpload());
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send(`/${req.method} ${req.url}`);
-});
-
 app.post("/upload", (req, res) => {
-    console.log(req.files.file);
-    res.send(`${req.files.file.name} was successfully uploaded`);
+    res.send(`¡El archivo: ${req.files.file.name}, fue cargado exitosamente!`);
+    res.status(200);
+    let keys = Object.keys(req.files.file);
+    console.log(`¡El archivo: ${req.files.file.name}, fue cargado exitosamente!`);
+    console.log(`\nPuede ver más información útil iterando por los siguientes elementos del objeto 'req.files.file':`);
+    keys.map(key => console.log(key)); // Elementos para iterar
+    console.log('\nEjemplo:')
+    console.log('Tamaño del archivo: ' + req.files.file.size);
+    console.log('Tipo de archivo: ' + req.files.file.mimetype);
+    // console.log('MD5 del archivo: ' + req.files.file.md5);
+    // console.log('Función mover del archivo: ' + req.files.file.mv);
 });
 
 // Darle ruta al archivo cargado en el servidor
-app.use(express.static('uploads'));
-
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 })
